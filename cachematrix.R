a62e8560de23c5fb3c716f3e031dce6eefb9a80b
Mate Wagner <valueinrisk@gmail.com>
2019-04-23 08:28:21 +0200

    A proposed solution for the second programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function will use the scoping features of R to reduce cost of computing inverse matrices.
+## The first function will create a special matrix which copies the object oriented programming factory idea
+## to the scripting language.
 
-## Write a short comment describing this function
-
+## This function will store two matrices: the original one and the inverse one. set will create the inverse one and get will return its value
 makeCacheMatrix <- function(x = matrix()) {
-
+i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(inverse) i<<-inverse
+  getinverse<-function() i
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function is the 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i<-x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        } 
+        data<-x$get()
+        i<-solve(data)
+        x$setinverse(i)
+        i
 }
+
